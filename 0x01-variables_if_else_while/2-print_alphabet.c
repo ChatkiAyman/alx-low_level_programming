1f065d3232cb046a8952b898ce44497dcfec3fe7
ChatkiAyman <aymanchatki4@gmail.com>
2024-01-04 18:40:24 +0100

    Modify 2-print_alphabet.c file

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,19 @@
+
 #include <stdio.h>
+
 /**
- * main - Prints the alphabet in lowercase, followed by a new line.
+ * main - Prints the alphabet.
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (int i = 0; i < 26; i++)
+	for (i = 0; i < 26; i++)
 	{
-		putchar(alpha[i]);
-
+		putchar(alp[i]);
 	}
 	putchar('\n');
 	return (0);
