f2c2d084ed2537e5499db20868d1744d01c7fcdc
ChatkiAyman <aymanchatki4@gmail.com>
2024-01-05 18:32:06 +0100

    Fix

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet from z to a
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 26; i >= 0; i--)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
