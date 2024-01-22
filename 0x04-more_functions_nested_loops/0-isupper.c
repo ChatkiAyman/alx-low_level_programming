d8e5fc1dbb94ca24e22421771010c4aa0aec0c91
ChatkiAyman <aymanchatki4@gmail.com>
2024-01-22 18:03:35 +0100

    Modify Betty

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,21 +2,22 @@
 #include <stdio.h>
 
 /**
-* _isupper: checks for upper case letter
-*
-* Return: 0 or 1
-*/
+ * _isupper - checks for upper case letter
+ * @c: take number
+ *
+ * Return: 0 or 1
+ */
 
 int _isupper(int c)
 {
-  if (c >= 'A' && c <= 'Z')
-  {
-    return (1);
-  }
-  else
-{
-    return (0);
-  }
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 
 
